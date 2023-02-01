c5481622426b3c97c902a5052c706abedd6788b5
Rustam Astafeev <astafeev0308@gmail.com>
2023-02-01 18:03:56 +0500

    1 of initial commits

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='dj_ms_core',
+    name='dj-ms-core',
     version='0.0.1',
     packages=find_packages(),
     scripts=['manage.py', ]

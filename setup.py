e55b713fc90f6ed5f5afd90553fc3a5d20ffa304
Rustam Astafeev <astafeev0308@gmail.com>
2023-02-02 13:27:56 +0500

    Some changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dj-ms-core',
-    version='0.0.1',
+    version='0.0.4',
     packages=find_packages(),
     scripts=['manage.py', ]
 )

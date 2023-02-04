81dfd6fcd3b4f83b0d4a086262baab286af2e1b5
Rustam Astafeev <astafeev0308@gmail.com>
2023-02-04 20:52:57 +0500

    added some settings for static / media files

--- core/settings.py
+++ core/settings.py
@@ -195,6 +195,15 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets'
+]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

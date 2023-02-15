61aa347a0f15c12d4b2563397902b831d67a12e3
Рустам Астафеев <astafeev0308@gmail.com>
2023-02-15 23:58:37 +0500

    smol changes

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,11 @@ ALLOWED_HOSTS = [
     'localhost'
 ]
 
-ALLOWED_HOSTS += os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+extra_allowed_hosts = os.getenv('DJANGO_ALLOWED_HOSTS', None)
+
+if extra_allowed_hosts:
+    assert isinstance(extra_allowed_hosts.split(','), list)
+    ALLOWED_HOSTS.extend(extra_allowed_hosts.split(','))
 
 CORS_ORIGIN_ALLOW_ALL = False
 
@@ -56,7 +60,11 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
 ]
 
-CSRF_TRUSTED_ORIGINS += os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS', '').split(',')
+extra_csrf_trusted_origins = os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS', None)
+
+if extra_csrf_trusted_origins:
+    assert isinstance(extra_csrf_trusted_origins.split(','), list)
+    CSRF_TRUSTED_ORIGINS.extend(extra_csrf_trusted_origins.split(','))
 
 # Application definition
 
